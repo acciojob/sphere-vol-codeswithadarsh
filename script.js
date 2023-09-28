65fac6762e4ed425ea6dc205f4752917995e095e
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-28 17:53:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// Function to calculate the volume of a sphere
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  // Get the radius value entered by the user
+  var radiusInput = document.getElementById('radius');
+  var radius = parseFloat(radiusInput.value);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  // Check if the radius is a valid number
+  if (!isNaN(radius) && radius >= 0) {
+    // Calculate the volume of the sphere (V = (4/3) * π * r^3)
+    var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+
+    // Display the calculated volume in the volume field
+    var volumeField = document.getElementById('volume');
+    volumeField.value = volume.toFixed(2); // Displaying the volume rounded to 2 decimal places
+  } else {
+    // If the user entered an invalid radius, display an error message
+    var volumeField = document.getElementById('volume');
+    volumeField.value = 'Invalid input';
+  }
+}
