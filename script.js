700ded07126227cb109544992287ca842cc573ef
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-28 19:41:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function volume_sphere(e) {
-	e.preventDefault();
-    let r  = document.getElementById("radius").value;
-	let ans = (4/3)*(Math.PI)*(r*r*r)
-	 document.getElementById("volume").value= `${ans}`;
-  
-} 
+    e.preventDefault();
+    let r = document.getElementById("radius").value;
+    let ans = (4 / 3) * Math.PI * Math.pow(r, 3);
+
+    // Round the calculated volume to 4 decimal places
+    ans = ans.toFixed(4);
+
+    document.getElementById("volume").value = `${ans}`;
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
